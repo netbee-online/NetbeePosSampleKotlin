f6572a1c15904e231646bb80655914035c854f3e
Netbee <mr.estek@yahoo.com>
2024-04-17 10:29:21 +0330

    change version to 1.1.1 (3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "online.netbee.pos.sample"
         minSdk = 23
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
