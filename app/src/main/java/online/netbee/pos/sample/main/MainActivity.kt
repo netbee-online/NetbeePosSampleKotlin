d1841ecf83712bbbe28e68db01c478df24fb57d7
Netbee <mr.estek@yahoo.com>
2024-04-20 10:15:37 +0330

    add description

--- app/src/main/java/online/netbee/pos/sample/main/MainActivity.kt
+++ app/src/main/java/online/netbee/pos/sample/main/MainActivity.kt
@@ -21,7 +21,7 @@ import org.json.JSONObject
  * how to send and receive data. It has been developing in the simplest way. We have been avoiding
  * to use third party libraries for the sake of simplicity. So you must develop your app in your way.
  * If you are a C#, Dart or other language developer, this sample can give you a better view of how
- * you develop your own. Before you proceed further, make sure that you have completely read and
+ * you can develop your own. Before you proceed further, make sure that you have completely read and
  * understood the protocol document.
  *
  * @see <a href="https://github.com/netbee-online/NetbeePos>NetbeePos document</a>
